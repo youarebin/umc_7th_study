6a6fd1207a4d058257690906a00c3c810efd1687
kayubin <kayubin@gmail.com>
2024-10-15 00:38:08 +0900

    스타일 변경

--- movie-project/src/pages/movies.jsx
+++ movie-project/src/pages/movies.jsx
@@ -20,17 +20,8 @@ const Movies = () => {
 export default Movies;
 
 const Container = styled.div`
-    width: calc(100vw - 200px); // SideBar 너비만큼 제외
-    height: calc(100vh - 70px); // Navbar 높이만큼 제외
-    background-color: black;
-    margin-top: 70px; // Navbar 높이만큼 여백 추가
-    margin-left: 200px; // SideBar 너비만큼 여백 추가
-    margin-right: 60px;
-    color: white;
-    overflow-y: auto;
-    padding: 30px 30px;
-
     .title {
+        color: white;
         font-size: 30px;
         font-weight: bold;
         margin-bottom: 20px;
