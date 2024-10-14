4274a6061d2da75b38d7255df1ac0c648f45ab07
kayubin <kayubin@gmail.com>
2024-10-15 00:39:08 +0900

    api호출 customHook사용하여 변경

--- movie-project/src/pages/up-coming.jsx
+++ movie-project/src/pages/up-coming.jsx
@@ -1,58 +1,36 @@
 import styled from "styled-components";
-import axios from 'axios';
-import { useState, useEffect } from "react";
 import MovieCard from "../components/movie-card";
+import useCustomFetch from "../hooks/useCustomFetch";
 
 const UpComing = () => {
-    const [movies, setMovies] = useState([]);
-
-    useEffect(() => {
-        const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/movie/upcoming',
-        params: {language: 'ko', page: '1'},
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNjc0YjFmYTE0ZGJkYjhkYzk4YzViNDRkYTUxMzU5MiIsIm5iZiI6MTcyODIyOTQ1MC43NDQxMTcsInN1YiI6IjY3MDJhZjUwMTU5MmVmMWJhOTg1OTMwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.E5tumsVwwMuY-JQCObzRB2tNF_19fyLEXZSSWQ1fnIw'
-        }
-        };
-
-        axios
-        .request(options)
-        .then(function (response) {
-            console.log(response.data);
-            setMovies(response.data.results)
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-    },[]);
-
+    const {data: movies, isLoading, isError} = useCustomFetch(`/movie/now_playing?language=ko-kr&page=1`);
+    if (isLoading) {
+        return(
+            <div>
+                <h1>로딩중 입니다.</h1>
+            </div>
+        );
+    }
+    if (isError) {
+        return(
+            <div>
+                <h1>에러</h1>
+            </div>
+        );
+    }
     return (
-        <Container>
-            <MoviesContainer>
-            {movies?.map((movie) => (
+        <MoviesContainer>
+            {movies.data?.results.map((movie) => (
                 <MovieCard 
                     key = {movie.id}
-                    poster = {movie.poster_path} 
-                    title = {movie.title}
-                    releaseDate = {movie.release_date}
+                    movie = {movie}
                 />
             ))}
-            </MoviesContainer>
-        </Container>
+        </MoviesContainer>
     );
 };
 
 export default UpComing;
-
-const Container = styled.div`
-    height: calc(100vh - 70px); // Navbar 높이만큼 제외
-    background-color: black;
-    margin-top: 70px; // Navbar 높이만큼 여백 추가
-    margin-left: 200px; // SideBar 너비만큼 여백 추가
-    padding: 20px 20px;
-`;
 
 const MoviesContainer = styled.div`
     display: grid;
