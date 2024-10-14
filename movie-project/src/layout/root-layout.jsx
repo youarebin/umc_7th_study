0d0d13568989341291d781afbeda48dba5a054c1
kayubin <kayubin@gmail.com>
2024-10-15 00:37:45 +0900

    root 스타일 변경

--- movie-project/src/layout/root-layout.jsx
+++ movie-project/src/layout/root-layout.jsx
@@ -1,15 +1,34 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 import Navbar from "../components/navbar";
 import SideBar from "../components/side-bar";
 
 const RootLayout = () => {
     return (
         <>
-            <SideBar/>
-            <Navbar />
-            <Outlet />
+            <Navbar />     
+            <MainContentWrapper>
+                <SideBar />
+                <ContentWrapper>
+                    <Outlet />
+                </ContentWrapper>
+            </MainContentWrapper>
         </>
     );
 ;}
 
 export default RootLayout;
+
+const MainContentWrapper = styled.div`
+    display: flex;
+    margin-top: 70px; /* Navbar height */
+`;
+
+const ContentWrapper = styled.div`
+    flex-grow: 1;
+    min-height: 100vh; /* 최소 높이 설정 */
+    overflow-y: auto; /* 세로 스크롤 허용 */
+    padding: 20px;
+    margin-left: 200px; /* Sidebar width */
+    background-color: black;
+`;
