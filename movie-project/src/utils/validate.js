2da0496627e7a3af140d6aa901d3764c2ac20e48
kayubin <kayubin@gmail.com>
2024-10-28 16:02:32 +0900

    로그인,회원가입 validate

---
+++ movie-project/src/utils/validate.js
@@ -0,0 +1,55 @@
+const emailPattern = /^\w+([.-]?\w+)@\w+([.-]?\w+)(\.\w{2,3})+$/;
+
+function validateUser(values) {
+    const errors = {
+        email: '',
+        password: '',
+    }
+
+
+    if(typeof values.email != 'string') {
+        errors.email = "문자열이어야 합니다."
+    }
+    if(emailPattern.test(values.email) === false) {
+        errors.email = "유효한 이메일 형식이어야 합니다."
+    }
+    if (!values.email){
+        errors.email = "이메일은 필수 입력요소입니다."
+    }
+
+    if(typeof values.password != 'string') {
+        errors.password = "문자열이어야 합니다."
+    }
+    if (!values.password){
+        errors.password = "비밀번호는 필수 입력요소입니다."
+    }
+    if (values.password && values.password.length < 8){
+        errors.password = "비밀번호는 8자 이상이어야 합니다."
+    }
+    if (values.password.length > 16){
+        errors.password = "비밀번호는 16자 이하여야 합니다."
+    }
+
+    return errors;
+    
+}
+
+function validateLogin(values) {
+    return validateUser(values);
+}
+
+const validateSignup = (values) => {
+    const errors = validateUser(values);
+    const signupErrors = {...errors, passwordCheck: ''};
+  
+    if(!values.passwordCheck){
+        signupErrors.passwordCheck = '비밀번호 검증 또한 필수 입력요소입니다.'
+    }
+    if (values.password !== values.passwordCheck) {
+      signupErrors.passwordCheck = '비밀번호가 일치하지 않습니다.';
+    }
+  
+    return signupErrors;
+  };
+
+export {validateLogin, validateSignup}
