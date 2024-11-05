2df34f5a0be21769e1d10693f5763342919ecbec
kayubin <kayubin@gmail.com>
2024-11-06 00:24:19 +0900

    todoContext 생성

--- todoList-project/src/App.jsx
+++ todoList-project/src/App.jsx
@@ -1,40 +1,23 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import './App.css'
 import Input from './components/Input';
 import Button from './components/Button';
+import { TodoContext } from './context/TodoContext';
 
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1, task: '투두 만들어보기'},
-    {id: 2, task: '가나다라'},
-  ]);
-
-  const [text, setText] = useState('');
-  const [editingId, setEditingId] = useState('');
-  const [editText, setEditText] = useState('');
-
-  //랜더링 방지
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  }
-  //1.추가하기
-  const addTodo = () => {
-    setTodos((prev) => [
-      ...prev,
-      {id: Math.floor(Math.random() * 100) + 2, task: text},
-    ])
-  };
-  //2. 삭제하기
-  const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((item) => item.id != id))
-  };
-  //3. 수정하기 
-  const updateTodo = (id, text) => {
-    setTodos((prev) => 
-      prev.map((item) => (item.id === id ? {...item, task: text} : item))
-    );
-    setEditingId('');
-  };
+  const {
+    todos, 
+    text, 
+    setText, 
+    editingId, 
+    setEditingId, 
+    editText, 
+    setEditText,
+    handleSubmit, 
+    addTodo, 
+    deleteTodo, 
+    updateTodo
+  } = useContext(TodoContext);
 
   return (
     <>
--- todoList-project/src/main.jsx
+++ todoList-project/src/main.jsx
@@ -2,9 +2,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { TodoContextProvider } from './context/TodoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
+  <TodoContextProvider>
+    <App />    
+  </TodoContextProvider>
   // </StrictMode>,
 )
