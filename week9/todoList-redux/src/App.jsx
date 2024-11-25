4107eb6f38bee38dd3c64e12da7b4624492875fc
kayubin <kayubin@gmail.com>
2024-11-26 00:06:31 +0900

    todoList-redux 완성

--- week9/todoList-redux/src/App.jsx
+++ week9/todoList-redux/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+import InputTodo from "./components/InputTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div>
+      <InputTodo/>
+      <TodoList/>
+    </div>
+  );
 }
 
-export default App
+export default App;
